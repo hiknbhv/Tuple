aeca1074ebe95dd442b0140441247a94fbd4cb9a
Tamilea pimpton <pimptam0000@warrren.k12.in.us>
2024-11-04 10:18:23 -0500

    push

---
+++ main.py
@@ -0,0 +1,16 @@
+fruit_tuple = ("apple", "banana", "cherry", "date")
+print(fruit_tuple[0])
+print(fruit_tuple[-1])
+slice_tuple = fruit_tuple[1:3]
+print(slice_tuple)
+
+num_tuple = (3, 5, 3, 2, 8, 3, 7)
+print(num_tuple.count(3))
+print(num_tuple.index(8))
+
+person_info = ("Alice", "engineer", 28)
+name, job, age = person_info
+print(name)
+print(job)
+print(age)
+
